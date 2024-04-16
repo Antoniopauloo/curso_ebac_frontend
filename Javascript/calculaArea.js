74f7a86acd3afd3258a308facd2e1a87ccff3229
Antonio Paulo Camarda dos Santos <90860420+Antoniopauloo@users.noreply.github.com>
2024-04-16 13:18:27 -0300

    Desafio exercicio de Calculadora área JS

---
+++ Javascript/calculaArea.js
@@ -0,0 +1,9 @@
+let base = prompt("Digite a base:");
+let altura = prompt("Digite a altura:");
+
+function calculaArea(base, altura) {
+    area = base * altura
+    return area;
+}
+
+alert("A área do retângulo é: " + calculaArea(base, altura));
