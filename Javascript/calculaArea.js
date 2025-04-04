2c126d534d926d6a728451b2d0fe1395c3fc4927
Antonio <antoniopaulocamarda@gmail.com>
2025-04-04 00:58:35 -0300

    Desafio Scss

--- Javascript/calculaArea.js
+++
@@ -1,9 +0,0 @@
-let base = prompt("Digite a base:");
-let altura = prompt("Digite a altura:");
-
-function calculaArea(base, altura) {
-    area = base * altura
-    return area;
-}
-
-alert("A área do retângulo é: " + calculaArea(base, altura));
