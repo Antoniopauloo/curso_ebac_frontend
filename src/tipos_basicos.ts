f42e6cb51ccd603b717d1970895f62f551161849
Antonio <antoniopaulocamarda@gmail.com>
2025-04-15 23:46:52 -0300

    Primeiro commit

---
+++ src/tipos_basicos.ts
@@ -0,0 +1,26 @@
+let estaChovendo: boolean = false
+
+estaChovendo = true
+
+let idade: number = 36
+
+let altura: number = 1.77
+
+const nacionalidade: string = 'brasileira'
+
+const colegas: string[] = ['lucas', 'fernanda']
+const tecnologias: Array<string> = ['html', 'css', 'js'];
+const notas: ReadonlyArray<number> = [7, 9, 5, 8];
+
+const listas: [nome: string, estaEstudando: boolean, idade: number] = ['antonio', true, 36]
+
+let idadeDaAna: number | string = 25
+idadeDaAna = '25 anos'
+
+let dadosDaApi: any;
+dadosDaApi = 10
+dadosDaApi = true
+dadosDaApi = [1,2,3]
+dadosDaApi = 'string'
+
+let curso = 'front-end'
